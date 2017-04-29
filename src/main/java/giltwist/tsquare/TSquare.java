2ba31ba2d69018a0a746b3ea1f59b4ab4f4c6bbf
Giltwist <giltwist@hotmail.com>
2017-04-28 21:34:41 -0400

    Added delete tool

--- src/main/java/giltwist/tsquare/TSquare.java
+++ src/main/java/giltwist/tsquare/TSquare.java
@@ -27,7 +27,7 @@ public class TSquare {
 
     public static final String MODID = "tsquare";
     public static final String MODNAME = "T-Square Builder Tools";
-    public static final String MODVERSION = "0.9.0";
+    public static final String MODVERSION = "0.9.1";
     public static Set<String> USERWHITELIST;
     public static Set<Block> BLOCKBLACKLIST;
 
